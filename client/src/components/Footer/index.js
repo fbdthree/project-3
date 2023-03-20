e2f01aabad8b45f5e57d1f7ff198c24296ee0f85
fbdthree <112531356+fbdthree@users.noreply.github.com>
2023-03-20 18:38:33 -0400

    Added a random pokemon and image fetched from the pokeapi

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,161 @@
-import React from 'react';
+// import React from 'react';
+// import { useLocation, useNavigate } from 'react-router-dom';
+
+// const Footer = () => {
+//   const location = useLocation();
+//   const navigate = useNavigate();
+//   return (
+//     <footer className="w-100 mt-auto bg-secondary p-4">
+//       <div className="container text-center mb-5">
+//         {location.pathname !== '/' && (
+//           <button
+//             className="btn btn-dark mb-3"
+//             onClick={() => navigate(-1)}
+//           >
+//             &larr; Go Back
+//           </button>
+//         )}
+//         <h4>
+//           Made with{' '}
+//           <span
+//             className="emoji"
+//             role="img"
+//             aria-label="heart"
+//             aria-hidden="false"
+//           >
+//             ❤️
+//           </span>{' '}
+//           by the Tech Thoughts team.
+//         </h4>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+
+// import React, { useEffect, useState } from 'react';
+// import { useLocation, useNavigate } from 'react-router-dom';
+
+// const Footer = () => {
+//   const location = useLocation();
+//   const navigate = useNavigate();
+//   const [pokemonFact, setPokemonFact] = useState('');
+
+//   useEffect(() => {
+//     const fetchPokemonFact = async () => {
+//       const response = await fetch('https://pokeapi.co/api/v2/characteristic/1/');
+//       const data = await response.json();
+//       const randomFact = data.descriptions[Math.floor(Math.random() * data.descriptions.length)].description;
+//       setPokemonFact(randomFact);
+//     };
+//     fetchPokemonFact();
+//   }, []);
+
+//   return (
+//     <footer className="w-100 mt-auto bg-secondary p-4">
+//       <div className="container text-center mb-5">
+//         {location.pathname !== '/' && (
+//           <button
+//             className="btn btn-dark mb-3"
+//             onClick={() => navigate(-1)}
+//           >
+//             &larr; Go Back
+//           </button>
+//         )}
+//         <h2>Random Pokemon Fact: {pokemonFact}</h2>
+//         <h4>
+//           Made with{' '}
+//           <span
+//             className="emoji"
+//             role="img"
+//             aria-label="heart"
+//             aria-hidden="false"
+//           >
+//             ❤️
+//           </span>{' '}
+//           by the Tech Thoughts team. 
+//         </h4>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+
+// import React, { useEffect, useState } from 'react';
+// import { useLocation, useNavigate } from 'react-router-dom';
+
+// const Footer = () => {
+//   const location = useLocation();
+//   const navigate = useNavigate();
+//   const [randomPokemon, setRandomPokemon] = useState('');
+
+//   useEffect(() => {
+//     const fetchRandomPokemon = async () => {
+//       const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1118');
+//       const data = await response.json();
+//       const randomIndex = Math.floor(Math.random() * data.results.length);
+//       const randomPokemonUrl = data.results[randomIndex].url;
+//       const pokemonResponse = await fetch(randomPokemonUrl);
+//       const pokemonData = await pokemonResponse.json();
+//       setRandomPokemon(pokemonData.name);
+//     };
+//     fetchRandomPokemon();
+//   }, []);
+
+//   return (
+//     <footer className="w-100 mt-auto bg-secondary p-4">
+//       <div className="container text-center mb-5">
+//         {location.pathname !== '/' && (
+//           <button
+//             className="btn btn-dark mb-3"
+//             onClick={() => navigate(-1)}
+//           >
+//             &larr; Go Back
+//           </button>
+//         )}
+//         <h4>
+//           Made with{' '}
+//           <span
+//             className="emoji"
+//             role="img"
+//             aria-label="heart"
+//             aria-hidden="false"
+//           >
+//             ❤️
+//           </span>{' '}
+//           by the Tech Thoughts team. Random Pokemon: {randomPokemon}
+//         </h4>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [randomPokemon, setRandomPokemon] = useState('');
+  const [randomPokemonImageUrl, setRandomPokemonImageUrl] = useState('');
+
+  useEffect(() => {
+    const fetchRandomPokemon = async () => {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1118');
+      const data = await response.json();
+      const randomIndex = Math.floor(Math.random() * data.results.length);
+      const randomPokemonUrl = data.results[randomIndex].url;
+      const pokemonResponse = await fetch(randomPokemonUrl);
+      const pokemonData = await pokemonResponse.json();
+      setRandomPokemon(pokemonData.name);
+      setRandomPokemonImageUrl(pokemonData.sprites.front_default);
+    };
+    fetchRandomPokemon();
+  }, []);
+
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container text-center mb-5">
@@ -15,6 +167,7 @@ const Footer = () => {
             &larr; Go Back
           </button>
         )}
+        
         <h4>
           Made with{' '}
           <span
@@ -25,11 +178,16 @@ const Footer = () => {
           >
             ❤️
           </span>{' '}
-          by the Tech Thoughts team.
+          by the Pokemon team. 
         </h4>
+        <h2>Random Pokemon: {randomPokemon}</h2>
+        {randomPokemonImageUrl && (
+          <img src={randomPokemonImageUrl} alt={`A picture of ${randomPokemon}`} />
+        )}
       </div>
     </footer>
   );
 };
 
 export default Footer;
+
