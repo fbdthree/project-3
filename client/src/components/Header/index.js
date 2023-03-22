238f83a4b5bfe57d1c028cc79b49a88822ac334d
monicapong <111400935+monicapong@users.noreply.github.com>
2023-03-22 01:58:21 -0400

    fixed header for logged in auth

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -9,26 +9,35 @@ const Header = () => {
     Auth.logout();
   };
   return (
-<div>
+    <div>
       <img className='block m-auto mt-4 w-60' src='assets/images/pokemanlogo.png' />
       <div className="container mx-auto mt-10 px-4 bg-slate-200 rounded-xl">
         <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="lg:flex lg:gap-x-12">
-            <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+            <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">
               Home
-            </a>
-            <a href="discussionBoard" className="text-sm font-semibold leading-6 text-gray-900">
-              Discussion Board
-            </a>
-            <a href="/dashBoard" className="text-sm font-semibold leading-6 text-gray-900">
-              Dashboard
-            </a>
+            </Link>
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="/login" className="text-sm font-semibold leading-6 text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/me" className="mx-3 text-sm font-semibold leading-6 text-gray-900">
+                Dashboard
+              </Link>
+              <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                <a onClick={logout} className="text-sm font-semibold leading-6 text-gray-900">
+                  Logout <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                <Link to="/login" className="text-sm font-semibold leading-6 text-gray-900">
+                  Log in <span aria-hidden="true">&rarr;</span>
+                </Link>
+              </div>
+            </>
+          )}
         </nav>
       </div>
     </div>
