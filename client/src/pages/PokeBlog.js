19892683f22f2aba24c11c0658f5bfdc605a908c
CodingJarrett <codingjarrett@gmail.com>
2023-03-22 04:08:14 -0400

    added css for poke post form

--- client/src/pages/PokeBlog.js
+++ client/src/pages/PokeBlog.js
@@ -1,26 +1,34 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import PokePostList from '../components/PokePostList';
+import PokePostForm from '../components/PokePostForm';
+
 import { QUERY_POKEPOSTS } from '../utils/queries';
 
 const PokeBlog = () => {
-  const { loading, data } = useQuery(QUERY_POKEPOSTS);
-  const pokePosts = data?.pokePosts || [];
+    const { loading, data } = useQuery(QUERY_POKEPOSTS);
+    const pokePosts = data?.pokePosts || [];
 
-  return (
-    <main>
-      <div className="flex-row justify-center container mt-10 bg-slate-200 rounded-xl">
-        <PokePostForm />
-        <div className="container mx-auto mt-10 px-4 bg-slate-200 rounded-xl">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <PokePostList
-              pokePosts={pokePosts}
-              title="Some Feed for PokePosts(s)..."
-            />
-          )}
-        </div>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <div className="flex-row justify-center container mt-4 bg-slate-200 rounded-xl">
+                <PokePostForm />
+            </div>
+            <div>
+                <div className="container mx-auto mt-10 px-4 bg-slate-200 rounded-xl">
+                    {loading ? (
+                        <div>Loading...</div>
+                    ) : (
+                        <PokePostList
+                            pokePosts={pokePosts}
+                            title="Some Feed for PokePosts(s)..."
+                        />
+                    )}
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default PokeBlog;
